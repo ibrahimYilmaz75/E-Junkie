0a775f8cc9212b96af79e60a55933697973fe9c2
BanuKhasanboeva <hbanu2617@gmail.com>
2024-09-27 16:08:46 +0300

    us_306 biti

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class BaseDriver {
-    public static WebDriver driver;
+    public static WebDriver  driver;
     public static WebDriverWait wait;
     public static JavascriptExecutor js;
     static  // tearStart()
